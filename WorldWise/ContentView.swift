790660e4a613244bbd8472e2c6428d2ce7d745a3
Sai Pappu <saivaishnavi.pappu@gmail.com>
2025-07-16 13:55:53 -0400

    added padding and colors to opening page for app

--- WorldWise/ContentView.swift
+++ WorldWise/ContentView.swift
@@ -9,40 +9,84 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            VStack{
-                Text("🌍 WorldWise")
-                    .font(.system(size:50))
-                    .bold()
-                    .padding()
-                Text("   Choose a country to explore! ")
-                    .padding(2)
-                    .font(.system(size:20))
-                
-                NavigationLink(destination: franceview()){
-                    Text("🇫🇷 FRANCE")
-                        .font(.system(size:25))
+        ZStack {
+            Color(red: 222 / 255.0, green: 229 / 255.0, blue: 212 / 255.0)
+                .ignoresSafeArea()
+
+            Rectangle()
+                .stroke(Color(red: 238 / 255.0, green: 239 / 255.0, blue: 224 / 255.0), lineWidth: 20)
+                .ignoresSafeArea()
+
+            NavigationStack {
+                VStack(spacing: 15) {
+                    Text("WorldWise 🌍")
+                        .font(.system(size:42, weight: .heavy, design: .rounded))
+                        .bold()
+                        .padding(15)
+                    Text("CHOOSE A COUNTRY TO EXPLORE! ")
+                        .padding(5)
+                        .font(.system(size:18, weight: .heavy, design: .rounded))
+                        .padding(.bottom, 2)
+
+                    NavigationLink(destination: franceview()){
+                        Text("🇫🇷 FRANCE")
+                            .font(.system(size:25, design: .monospaced))
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                            .background(Color(red: 255 / 255.0, green: 234 / 255.0, blue: 234 / 255.0))
+                            .cornerRadius(10)
+                    }
+
+                    NavigationLink(destination: germanyview()){
+                        Text("🇩🇪 GERMANY")
+                            .font(.system(size:25, design: .monospaced))
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                            .background(Color(red: 255 / 255.0, green: 234 / 255.0, blue: 234 / 255.0))
+                            .cornerRadius(10)
+                    }
+
+                    NavigationLink(destination: indiaview()){
+                        Text("🇮🇳 INDIA")
+                            .font(.system(size:25, design: .monospaced))
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                            .background(Color(red: 255 / 255.0, green: 234 / 255.0, blue: 234 / 255.0))
+                            .cornerRadius(10)
+                    }
+
+                    NavigationLink(destination: japanview()){
+                        Text("🇯🇵 JAPAN")
+                            .font(.system(size:25, design: .monospaced))
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                            .background(Color(red: 255 / 255.0, green: 234 / 255.0, blue: 234 / 255.0))
+                            .cornerRadius(10)
+                    }
+
+                    NavigationLink(destination: greeceview()){
+                        Text("🇬🇷 GREECE")
+                            .font(.system(size:25, design: .monospaced))
+                            .foregroundColor(.black)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 40)
+                            .background(Color(red: 255 / 255.0, green: 234 / 255.0, blue: 234 / 255.0))
+                            .cornerRadius(10)
+                    }
                 }
-                NavigationLink(destination: germanyview()){
-                    Text("🇩🇪 GERMANY")
-                        .font(.system(size:25))
-                }
-                NavigationLink(destination: indiaview()){
-                    Text("🇮🇳 INDIA")
-                        .font(.system(size:25))
-                }
-                NavigationLink(destination: japanview()){
-                    Text("🇯🇵 JAPAN")
-                        .font(.system(size:25))
-                }
+                .padding()
+                .background(Color.white)
+                .cornerRadius(25)
             }
-            NavigationLink(destination: greeceview()){
-                Text("🇬🇷 GREECE")
-                    .font(.system(size:25))
-            }
+            .padding(20)
         }
     }
 }
-    #Preview {
-        ContentView()
-    }
+
+#Preview {
+    ContentView()
+}
