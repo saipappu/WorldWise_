d743888125bf8f3e5d1c4596a6060556f8168988
Sai Pappu <saivaishnavi.pappu@gmail.com>
2025-07-16 10:35:17 -0400

    Initial Commit

---
+++ WorldWise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorldWise
+//
+//  Created by Sai Pappu on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorldWise/WorldWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldWiseApp.swift
+//  WorldWise
+//
+//  Created by Sai Pappu on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WorldWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
