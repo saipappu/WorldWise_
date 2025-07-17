c89d41d0965350ab6b55029200d7f60c61749f55
Krisha <krishaj0907@gmail.com>
2025-07-17 11:43:38 -0400

    france list added

---
+++ WorldWise/FRANCE/MmannerismsView.swift
@@ -0,0 +1,25 @@
+//
+//  MmannerismsView.swift
+//  WorldWise
+//
+//  Created by Meghana Laddha on 7/17/25.
+//
+
+import Foundation
+import SwiftUI
+struct MmannerismsView: View {
+    var body: some View {
+        ScrollView{ Spacer()
+            VStack (spacing: 20) {
+                
+                
+            }
+            .navigationTitle("Mannerisms")
+            
+        }
+    }
+}
+#Preview {
+    MmannerismsView()
+}
+
